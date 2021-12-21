b608ee1b1c89696f24a750dc16e4eb788d9e1448
shahid0 <shahidhussain0212@gmail.com>
2021-12-21 18:54:40 +0500

    src files

--- src/Components/ClassRoom/SideBar/SideBar.jsx
+++ src/Components/ClassRoom/SideBar/SideBar.jsx
@@ -1,48 +1,49 @@
-import classes from './SIdeBar.module.css';
+// import classes from './SIdeBar.module.css';
 // import { Icon,Button } from "atomize";
-import { useState } from 'react';
+// import { useState } from 'react';
 import ClasEs from '../Classes/Classes'
 
 
-const DeskT = () => {
-    const [sel, PathSel] = useState("Classes")
-    return (
-        <>
-            <div className={classes.cont}>
-                <h1>
-                    E-Learn
-                </h1>
-                <div className={classes.menuC}>
-                    <h6
-                        onClick={() => PathSel("Classes")}
-                        className={sel === "Classes" ? classes.chkd : ""}
-                    >
-                        Classes
-                    </h6 >
-                    <h6
-                        onClick={() => PathSel("Quiz")}
-                        className={sel === "Quiz" ? classes.chkd : ""}
-                    >
-                        Qiuz
-                    </h6>
-                    <h6
-                        onClick={() => PathSel("Assignments")}
-                        className={sel === "Assignments" ? classes.chkd : ""}
-                    >
-                        Assignments
-                    </h6>
-                </div>
-            </div>
-            <ClasEs />
-        </>
+// const DeskT = () => {
+//     const [sel, PathSel] = useState("Classes")
+//     return (
+//         <>
+//             <div className={classes.cont}>
+//                 <h1>
+//                     E-Learn
+//                 </h1>
+//                 <div className={classes.menuC}>
+//                     <h6
+//                         onClick={() => PathSel("Classes")}
+//                         className={sel === "Classes" ? classes.chkd : ""}
+//                     >
+//                         Classes
+//                     </h6 >
+//                     <h6
+//                         onClick={() => PathSel("Quiz")}
+//                         className={sel === "Quiz" ? classes.chkd : ""}
+//                     >
+//                         Qiuz
+//                     </h6>
+//                     <h6
+//                         onClick={() => PathSel("Assignments")}
+//                         className={sel === "Assignments" ? classes.chkd : ""}
+//                     >
+//                         Assignments
+//                     </h6>
+//                 </div>
+//             </div>
+//             <ClasEs />
+//         </>
 
-    )
-}
+//     )
+// }
 
 const SideBar = () => {
     return (
         <>
-            <DeskT />
+            {/* <DeskT /> */}
+            <ClasEs />
         </>
     )
 }
--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -32,14 +32,14 @@ const Info = ({ RouteChange }) => {
             <div className={classes.twoBox}>
 
                 <div className={classes.BoxOne}>
-                    <div>
+                    <div className={classes.stg}>
                         <h1>Heading</h1>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi dolor voluptatem consectetur totam dicta, quos nemo fugiat? Cum officia dolorum accusamus, eos perferendis molestiae corrupti optio aliquid vitae doloribus!
                     </div>
                 </div>
                 <div className={classes.line}></div>
                 <div className={classes.BoxSec}>
-                    <div>
+                    <div className={classes.stg}>
 
                         <h1>Heading</h1>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores quaerat, suscipit doloremque corrupti, autem facere ex ad inventore cupiditate iste eveniet voluptate quod asperiores ratione dolores. Minus iure possimus fuga?
